f0dd68f6f84125fec4f34a3818c4673eb2287147
Ruikai Li <42368961+liruikaigx@users.noreply.github.com>
2018-09-06 14:36:04 +1000

    Comment
    Add some comment into this code.

--- lab_6.py
+++ lab_6.py
@@ -10,10 +10,10 @@ except ValueError:
     print('Incorrect input, giving up.')
     sys.exit()
 
-for i in range(2, N + 1):
+for i in range(2, N + 1):               
     sum1 = 0
-    for s in range(1, i):
-        if i % s == 0:
-            sum1 += s
+    for s in range(1, i):         
+        if i % s == 0:                #find the number which can divide i
+            sum1 += s                 #sum them up and check
     if sum1 == i:
         print(f'{i} is a perfect number.')
